ad27a164156c0882fb34001faf13ea6656fb8818
therajsoni <rajsoni968392@gmail.com>
2026-02-18 18:41:15 +0000

    coomit

---
+++ kind-config.sh
@@ -0,0 +1,9 @@
+# Create kind cluster configuration
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+EOF
